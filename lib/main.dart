f2f6ae5763a3edc0a912357f85617f5455e9c318
shvn-iwa <73055963+shvn-iwa@users.noreply.github.com>
2021-12-21 01:03:25 +0900

    themecolor を設定。

--- lib/main.dart
+++ lib/main.dart
@@ -5,22 +5,16 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: customSwatch,
+        primaryColor: customSwatch[500],
+        accentColor: customSwatch[800],
+        primaryColorLight: customSwatch[100],
+        backgroundColor: customSwatch[50],
       ),
       home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
@@ -44,6 +38,22 @@ class MyHomePage extends StatefulWidget {
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
+
+const MaterialColor customSwatch = MaterialColor(
+  0x23580C,
+  <int, Color>{
+    50: Color(0xFFE5EBE2),
+    100: Color(0xFFBDCDB6),
+    200: Color(0xFF91AC86),
+    300: Color(0xFF658A55),
+    400: Color(0xFF447130),
+    500: Color(0xFF23580C),
+    600: Color(0xFF1F500A),
+    700: Color(0xFF1A4708),
+    800: Color(0xFF153D06),
+    900: Color(0xFF0C2D03),
+  },
+);
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
