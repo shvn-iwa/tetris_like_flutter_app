beab3f1f517242dbcadb71cc46042c4afd5ce12c
shvn-iwa <73055963+shvn-iwa@users.noreply.github.com>
2022-02-16 22:43:55 +0900

    change themecolor.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:tetris_like_flutter_app/theme.dart';
 
 void main() {
   runApp(MyApp());
@@ -19,23 +20,6 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
-
-const MaterialColor customSwatch = MaterialColor(
-  0xFF23580C,
-  <int, Color>{
-    50: Color(0xFFE5EBE2),
-    100: Color(0xFFBDCDB6),
-    200: Color(0xFF91AC86),
-    300: Color(0xFF658A55),
-    400: Color(0xFF447130),
-    500: Color(0xFF23580C),
-    600: Color(0xFF1F500A),
-    700: Color(0xFF1A4708),
-    800: Color(0xFF153D06),
-    900: Color(0xFF0C2D03),
-  },
-);
 
   @override
   Widget build(BuildContext context) {
--- lib/theme.dart
+++ lib/theme.dart
@@ -1,1 +1,17 @@
 import 'package:flutter/material.dart';
+
+const MaterialColor customSwatch = MaterialColor(
+  0xFF23580C,
+  <int, Color>{
+    50: Color(0xFFE5EBE2),
+    100: Color(0xFFBDCDB6),
+    200: Color(0xFF91AC86),
+    300: Color(0xFF658A55),
+    400: Color(0xFF447130),
+    500: Color(0xFF23580C),
+    600: Color(0xFF1F500A),
+    700: Color(0xFF1A4708),
+    800: Color(0xFF153D06),
+    900: Color(0xFF0C2D03),
+  },
+);
